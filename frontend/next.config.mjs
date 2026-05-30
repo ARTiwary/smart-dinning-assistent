8419c8b1836a381f6f8166b96208128092a1257f
ARTiwary <ayushrajtiwary07@gmail.com>
2026-05-30 15:26:28 +0530

    ui update 2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  images: {
+    domains: ['images.unsplash.com', 'res.cloudinary.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
